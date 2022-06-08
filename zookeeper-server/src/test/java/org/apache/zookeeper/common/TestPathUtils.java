bb71f740abdcb24ae75a3286eb3947a75f62cf96
luca-capotombolo <capoluca98@gmail.com>
2022-06-08 10:19:54 +0200

    AGGIUNTA DI BA-DUA, PIT E IMPLEMENTAZIONE (1) DI PATH

---
+++ zookeeper-server/src/test/java/org/apache/zookeeper/common/TestPathUtils.java
@@ -0,0 +1,59 @@
+package org.apache.zookeeper.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@RunWith(Parameterized.class)
+public class TestPathUtils {
+    private final String inputValidPath;
+    private final String inputInvalidPath;
+
+    public TestPathUtils(String inputValidPath, String inputInvalidPath){
+        this.inputInvalidPath = inputInvalidPath;
+        this.inputValidPath = inputValidPath;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<?> primeNumbers() {
+        return Arrays.asList(new Object[][] {
+                {"/znode1", null},
+                {"/a/b", "a/b"},
+                {"/a/b/c", "a/b/c"},
+                {"/a", ""},
+        });
+    }
+
+    @Test
+    public void testInvalidPath(){
+
+        IllegalArgumentException error = null;
+
+        try{
+            PathUtils.validatePath(this.inputInvalidPath);
+        }catch (IllegalArgumentException e){
+            error = e;
+        }
+
+        Assert.assertNotNull(error);
+    }
+
+    @Test
+    public void testValidPath(){
+
+        IllegalArgumentException error = null;
+
+        try{
+            PathUtils.validatePath(this.inputValidPath);
+        }catch (IllegalArgumentException e){
+            error = e;
+        }
+
+        Assert.assertNull(error);
+    }
+}
---
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/TestDataTree.java
@@ -0,0 +1,11 @@
+package org.apache.zookeeper.server;
+
+import org.junit.Test;
+
+public class TestDataTree {
+
+    @Test
+    public void test(){
+
+    }
+}
